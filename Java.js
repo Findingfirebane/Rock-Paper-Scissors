29eadf486f74ba97a3543ccb7a2ee1ef978b7d77
Findingfirebane <141285352+Findingfirebane@users.noreply.github.com>
2023-11-02 12:59:41 -0400

    Compter turn worked on the function that makes computer decide. Stuck on getting it to randomly chose

---
+++ Java.js
@@ -0,0 +1,22 @@
+console.log("Good morning, Good evening and Goodnight")
+
+let x = 'Rock' ;
+let y = 'Scissors'
+let z = 'Paper'
+
+function getComputerChoice(z=1){
+    if (x === 1) {
+        return y ;
+    } else if (y === 1) {
+        return z;
+    } else (z === 1) 
+        return x ;
+    }
+
+    console.log(getComputerChoice(x=1))
+    console.log(getComputerChoice(y=1))
+    console.log(getComputerChoice(z=1))
+
+ 
+
+
