50c4610992c2c239a6047ef945deca5fe9ed3337
Findingfirebane <141285352+Findingfirebane@users.noreply.github.com>
2023-11-09 09:48:49 -0500

    added two game functions Adeed a score tracker function and a play game function

--- Java.js
+++ Java.js
@@ -1,7 +1,10 @@
-const randomNumber = Math.random()
-const computerSelection = '';
+//const randomNumber = Math.random()
+let computerSelection = '';
 
 function getComputerChoice() {
+    const randomNumber = Math.random()
+
+
     if (randomNumber >= 0 && randomNumber < 1/3){
         computerSelection ='rock';
     } else if (randomNumber >= 1/3 && randomNumber < 2/3){ 
@@ -12,12 +15,14 @@ function getComputerChoice() {
 
 
 
-
-//const playerMove = prompt('Rock,Paper or Scissors?')
 const playerMove = ''
 
-function playRound(){ 
-    const playerMove = prompt('Rock,Paper or Scissors?')
+function playRound(){
+   getComputerChoice()
+    
+    
+     // have to make prompt input case insensitive
+    const playerMove = prompt('rock, paper, scissors')
 
     if(playerMove === computerSelection){
         return 'you tied';
@@ -34,12 +39,64 @@ function playRound(){
     } else if(computerSelection === 'rock' && playerMove === 'scissors'){
         return 'Computer Picked Rock, you lost you fucking idiot.';
     } 
+}//create a function to determine if win, lose, or tie
+ 
+
+
+
+// game fucntion
+function game(){
+    console.log(playRound());
+    console.log(playRound());
+    console.log(playRound());
+    console.log(playRound());
+    console.log(playRound());
+
+}
+game()
+
+
+
+//win counter
+function scoreTracker(){
+    let playerScore = '0'
+    let computerScore = '0'
+    let tie = '0'
+if(playerMove == computerSelection){
+    console.log(tie++)
+} 
+else if (playerMove === 'rock'){
+    if(computerSelection ==='scissors'){
+        console.log('Player won');
+        playerScore++;
+    }
+    else{
+        console.log('Computer Won womp womp');
+        computerScore++
+    }
+} 
+else if (playerMove === 'scissors'){
+    if(computerSelection === 'paper'){
+        console.log('You Win muddersucker');
+        playerScore++;
+    }
+    else(computerSelection === 'rock'){
+        console.log('You lost again?');
+        computerScore++;
+    }
+else if (playerMove === 'paper'){
+    if (computerSelection === 'rock'){
+        console.log('You done won son!');
+        playerScore++;
+    } else (computerSelection === 'scissors'){
+        console.log('you lost fuck boi');
+        computerScore++;
+
+    }
+}
 }
 
-//console.log(playRound(playerMove, computerSelection))
-
-playRound()
-playRound()
-playRound()
 
 
+
+}
